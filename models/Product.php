e560908b1c8d2f4125116d0bf6dc187f2091096e
Etrama22 <muhazis73@gmail.com>
2024-05-26 23:13:27 +0700

    ProductModels

---
+++ models/Product.php
@@ -0,0 +1,52 @@
+<?php
+
+class Product
+{
+    private $conn;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM products");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id)
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM products WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($name, $image, $price)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO products (name, image, price) VALUES (:name, :image, :price)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':price', $price);
+        return $stmt->execute();
+    }
+
+    public function update($id, $name, $image, $price)
+    {
+        $stmt = $this->conn->prepare("UPDATE products SET name = :name, image = :image, price = :price WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':price', $price);
+        return $stmt->execute();
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->conn->prepare("DELETE FROM products WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
+}
