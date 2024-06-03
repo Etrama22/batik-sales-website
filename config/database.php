f672ebb5d38ca573ed52702763dee475f15ddfef
Etrama22 <muhazis73@gmail.com>
2024-06-03 14:22:47 +0700

    database connection

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "penjualan";
+$dbname = "BatikSalesDB";
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
